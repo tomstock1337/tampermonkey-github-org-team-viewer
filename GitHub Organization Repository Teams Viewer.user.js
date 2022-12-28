28020318b0f77dc02dcc961b7d0336247fb89918
MSI\thoma <thomas.stockwell@gmail.com>
2022-12-28 14:15:47 -0500

    Added support for organization main page, when repositories are listed.

--- GitHub Organization Repository Teams Viewer.user.js
+++ GitHub Organization Repository Teams Viewer.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         GitHub Organization Repository Teams Viewer
 // @namespace    http://www.thomasstockwell.com/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       Thomas Stockwell
 // @match        https://github.com/**
@@ -15,127 +15,145 @@
 /* globals jQuery, $, waitForKeyElements */
 
 (function() {
-    'use strict';
+	'use strict';
 
-    const pageRegex = /(https?:\/\/(www\.)?github\.com\/orgs\/)([^\/]*)\/repositories\/?/;
-    const urlOrgsBase = "https://www.github.com/orgs/";
-    const urlGitHubBase = "https://www.github.com";
-    const pill = '<span class="label v-align-middle" style="border-color:{color};color:white">{pilltext}</span>';
-    var pageURL = window.location.href;
+	const pageRepoRegex = /(https?:\/\/(www\.)?github\.com\/orgs\/)([^\/]*)\/repositories\/?/;
+	const pageOrgProfileRegex = /(https?:\/\/(www\.)?github\.com\/)([^\/]*)\/?/;
+	const urlOrgsBase = "https://www.github.com/orgs/";
+	const urlGitHubBase = "https://www.github.com";
+	const pill = '<span class="label v-align-middle" style="border-color:{color};color:white">{pilltext}</span>';
+	var pageURL = window.location.href;
+	var pageURLArray;
+	var org;
 
-    // listen for changes
-    setInterval(function()
-                {
-        if (pageURL != location.href)
-        {
-            // page has changed, set new page as 'current'
-            pageURL = location.href;
-            if (pageURL.match(pageRegex)){
-                addRepoTeams();
-            }
-        }
-    }, 500);
+	// listen for changes
+	setInterval(function()
+							{
+			if (pageURL != location.href)
+			{
+					// page has changed, set new page as 'current'
+					pageURL = location.href;
+					if (pageURL.match(pageRepoRegex)){
+							pageURLArray = pageRepoRegex.exec(pageURL);
+							//[0] = https://github.com/orgs/orgname/repositories
+							//[1] = https://github.com/orgs/
+							//[2] = www.
+							//[3] = orgname
+							org = pageURLArray[3];
+							addRepoTeams(org);
+					}
+					else if (pageURL.match(pageOrgProfileRegex)) {
+							var orgRepos = $('div#org-profile-repositories');
+							if(orgRepos.length>0){
+									pageURLArray = pageOrgProfileRegex.exec(pageURL);
+									//[0] = https://github.com/orgname
+                                    //[1] = https://github.com/
+									//[2] = www.
+									//[3] = orgname
+									org = pageURLArray[3];
+									addRepoTeams(org);
+							}
+					}
+			}
+	}, 500);
 
-    function addRepoTeams(){
-    var colors=["#F73F0C","#B50BD4","#003BEB","#0BD48F","#93F500"];
-    var debug = true;
-    var pageURLArray = pageRegex.exec(pageURL);
-    //[0] = https://github.com/orgs/umd-its/repositories
-    //[1] = https://github.com/orgs/
-    //[2] =
-    //[3] = orgname
-    var org = pageURLArray[3];
-    var orgTeams = [];
-    var stage1 = 'Processing';
-    var stage2cntTeamsProcessed = 0;
+	function addRepoTeams(org){
+			var colors=["#F73F0C","#B50BD4","#003BEB","#0BD48F","#93F500"];
+			var debug = true;
+			var orgTeams = [];
+			var stage1 = 'Processing';
+			var stage2cntTeamsProcessed = 0;
 
-    if(debug) console.log('Stage 1 Start');
+			if(debug) console.log('Stage 1 Start');
+            if(debug) console.log(urlOrgsBase+org+"/teams");
 
-    //Grab all teams of the organization.  Pagination may be an issue for larger orgs
-    GM_xmlhttpRequest({
-        method: "GET",
-        url: urlOrgsBase+org+"/teams",
-        onload: function(orgTeamsPage)
-        {
-           var orgTeamsPageCode = $.parseHTML(orgTeamsPage.responseText);
-           var links = $("div#org-teams ul li a[id^=team]",orgTeamsPageCode);
-           $.each(links,(obj=>{
-               var teamName = $('span',links[obj]).text()
-               var teamURL = $(links[obj]).attr('href');
-               var color = colors[obj];
-               orgTeams.push({Name:teamName,URL:teamURL,Color:color});
-           }));
-           stage1 = 'Finished';
-        }
-    });
 
-    // For each team, determine what repositories they have access to.  Pagination may be an issue for larger orgs
-    function stage2start() {
-        if(stage1!=='Finished') {
-            window.setTimeout(stage2start, 100); /* this checks the flag every 100 milliseconds*/
-        } else {
-            if(debug) console.log('Stage 2 Start');
-            if(debug) console.log(orgTeams);
+			//Grab all teams of the organization.  Pagination may be an issue for larger orgs
+			GM_xmlhttpRequest({
+					method: "GET",
+					url: urlOrgsBase+org+"/teams",
+					onload: function(orgTeamsPage)
+					{
+							var orgTeamsPageCode = $.parseHTML(orgTeamsPage.responseText);
+							var links = $("div#org-teams ul li a[id^=team]",orgTeamsPageCode);
+							$.each(links,(obj=>{
+									var teamName = $('span',links[obj]).text()
+									var teamURL = $(links[obj]).attr('href');
+									var color = colors[obj];
+									orgTeams.push({Name:teamName,URL:teamURL,Color:color});
+							}));
+                        console.log(orgTeams);
+							stage1 = 'Finished';
+					}
+			});
 
-            orgTeams.forEach((obj,i)=>{
-                if(debug) console.log(urlGitHubBase+obj.URL+"/repositories");
-                GM_xmlhttpRequest({
-                    method: "GET",
-                    url: urlGitHubBase+obj.URL+"/repositories",
-                    onload: function(orgTeamsPage)
-                    {
-                        var orgTeamsPageCode = $.parseHTML(orgTeamsPage.responseText);
-                        var links = $("div#org-team-repositories ul li a[data-hovercard-type='repository']",orgTeamsPageCode);
-                        var linkArray=[];
-                        $.each(links,(i2)=>{
-                            var teamRepoUrl = $(links[i2]).attr('href');
-                            linkArray.push(teamRepoUrl);
-                            // orgTeams.push({Name:teamName,URL:teamURL,Color:color});
-                        });
-                        orgTeams[i].Repos = linkArray;
-                        stage2cntTeamsProcessed++;
-                    }
-                });
-            });
+			// For each team, determine what repositories they have access to.  Pagination may be an issue for larger orgs
+			function stage2start() {
+					if(stage1!=='Finished') {
+							window.setTimeout(stage2start, 100); /* this checks the flag every 100 milliseconds*/
+					} else {
+							if(debug) console.log('Stage 2 Start');
+							if(debug) console.log(orgTeams);
 
-            //Add pills to the necessary pages to display what teams are a part of what repositories.
-            function stage3Start(){
-                if(stage2cntTeamsProcessed!=orgTeams.length) {
-                    window.setTimeout(stage3Start, 100); /* this checks the flag every 100 milliseconds*/
-                } else {
-                    if(debug) console.log('Stage 3 Start');
-                    console.log(orgTeams);
+							orgTeams.forEach((obj,i)=>{
+									if(debug) console.log(urlGitHubBase+obj.URL+"/repositories");
+									GM_xmlhttpRequest({
+											method: "GET",
+											url: urlGitHubBase+obj.URL+"/repositories",
+											onload: function(orgTeamsPage)
+											{
+													var orgTeamsPageCode = $.parseHTML(orgTeamsPage.responseText);
+													var links = $("div#org-team-repositories ul li a[data-hovercard-type='repository']",orgTeamsPageCode);
+													var linkArray=[];
+													$.each(links,(i2)=>{
+															var teamRepoUrl = $(links[i2]).attr('href');
+															linkArray.push(teamRepoUrl);
+															// orgTeams.push({Name:teamName,URL:teamURL,Color:color});
+													});
+													orgTeams[i].Repos = linkArray;
+													stage2cntTeamsProcessed++;
+											}
+									});
+							});
 
-                    //Adjustments of the UI on the repository page
-                    var orgRepos = $("div#org-repositories ul li div[itemType='http://schema.org/Code']");
-                    $.each(orgRepos,(i3)=>{
-                        var repoURL = $("a[data-hovercard-type='repository']",orgRepos[i3]).attr('href');
-                        var repoTeams = [];
-                        var pills = $('<div></div>');
+							//Add pills to the necessary pages to display what teams are a part of what repositories.
+							function stage3Start(){
+									if(stage2cntTeamsProcessed!=orgTeams.length) {
+											window.setTimeout(stage3Start, 100); /* this checks the flag every 100 milliseconds*/
+									} else {
+											if(debug) console.log('Stage 3 Start');
+											console.log(orgTeams);
 
-                        orgTeams.forEach((obj4,i4)=>{
-                            obj4.Repos.forEach((obj5,i5)=>{
-                                if(repoURL===obj5)
-                                {
-                                    repoTeams.push(obj4);
-                                }
-                            });
-                        });
+											//Adjustments of the UI on the repository page
+											var orgRepos = $("div#org-repositories ul li div[itemType='http://schema.org/Code']");
+											$.each(orgRepos,(i3)=>{
+													var repoURL = $("a[data-hovercard-type='repository']",orgRepos[i3]).attr('href');
+													var repoTeams = [];
+													var pills = $('<div></div>');
 
-                        repoTeams.forEach((obj4,i4)=>{
-                            pills.append($(pill.replace('{pilltext}',obj4.Name).replace('{color}',obj4.Color)));
-                        });
+													orgTeams.forEach((obj4,i4)=>{
+															obj4.Repos.forEach((obj5,i5)=>{
+																	if(repoURL===obj5)
+																	{
+																			repoTeams.push(obj4);
+																	}
+															});
+													});
 
-                        $(orgRepos[i3]).append(pills);
+													repoTeams.forEach((obj4,i4)=>{
+															pills.append($(pill.replace('{pilltext}',obj4.Name).replace('{color}',obj4.Color)));
+													});
 
-                    });
-                }
-            }
-            stage3Start();
-        }
-    }
-    stage2start();
-};
+													$(orgRepos[i3]).append(pills);
+
+											});
+									}
+							}
+							stage3Start();
+					}
+			}
+			stage2start();
+	};
 
 })();
 
